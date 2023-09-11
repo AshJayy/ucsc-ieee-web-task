586d18ac18b48ca75acc5523b84c3988eff95cb4
AshJayy <ashwinie.ja@gmail.com>
2023-09-12 01:40:48 +0530

    New component <Header>

--- src/pages/Header.js
+++ src/pages/Header.js
@@ -1,8 +1,20 @@
 import React from "react";
+import './Header.css';
+import logo from '../logos/logo.png';
 
 function Header(){
     return(
-        <></>
+        <header className="black-transparent">
+            <nav>
+            <img src={logo} alt="logo" id="logo" />
+                <ul>
+                    <li>About</li>
+                    <li>Timeline</li>
+                    <li>Prizes</li>
+                    <li>FAQ</li>
+                </ul>
+            </nav>
+        </header>
     );
 }
 
