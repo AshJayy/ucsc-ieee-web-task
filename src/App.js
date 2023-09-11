d1b017a223764d21b9cec2cdf2929c7693bbd7d0
AshJayy <ashwinie.ja@gmail.com>
2023-09-12 01:40:12 +0530

    Set up background

--- src/App.js
+++ src/App.js
@@ -3,9 +3,11 @@ import Header from './pages/Header';
 
 function App() {
   return (
-    <>
+    <div style={{backgroundImage:"url(/bg.jpg)", 
+    backgroundSize:"cover",
+    width: "100%"}}>
         <Header/>
-    </>
+    </div>
   );
 }
 
